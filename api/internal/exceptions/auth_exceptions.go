65f35c4905ff0fb9aff0dfe1980cc16e5512a094
Douglas Blank <douglasblank1@gmail.com>
2022-07-15 02:30:25 -0300

    feat: changes exceptions to strings

--- api/internal/exceptions/auth_exceptions.go
+++ api/internal/exceptions/auth_exceptions.go
@@ -1,7 +1,5 @@
 package exceptions
 
-import "errors"
-
-var ErrAuthorizationFailed = errors.New("authorization failed")
-var ErrInvalidPassword = errors.New("invalid password")
-var ErrSessionNotFound = errors.New("session not found")
+var ErrAuthorizationFailed = "authorization failed"
+var ErrInvalidPassword = "invalid password"
+var ErrSessionNotFound = "session not found"
