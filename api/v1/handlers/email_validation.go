7572edcec17b25ed62787f6429fbf09908cdb94f
Douglas Blank <douglasblank1@gmail.com>
2022-07-25 01:21:33 -0300

    feat: implemented email validation before login

---
+++ api/v1/handlers/email_validation.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	authService "gymn/internal/services/auth"
+	"gymn/v1/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Email struct {
+	Email string `json:"email" validate:"required"`
+}
+
+func EmailValidation(c *fiber.Ctx) error {
+	email := new(Email)
+
+	if err := utils.GetBody(email, c); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err)
+	}
+
+	response, err := authService.ValidateEmail(email.Email)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err)
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(response)
+}
--- api/v1/routes/auth.go
+++ api/v1/routes/auth.go
@@ -7,5 +7,6 @@ import (
 )
 
 func AuthRoutes(app *fiber.App) {
+	app.Post("/email-validation", handlers.EmailValidation)
 	app.Post("/sign-in", handlers.SignInHandler)
 }
