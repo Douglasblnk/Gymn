3922309d1d810c7e378cfc2fd43977bc6119ac8f
Douglas Blank <douglasblank1@gmail.com>
2022-07-31 09:13:37 -0300

    feat: implement aws session credentials

---
+++ api/internal/providers/aws/aws_client.go
@@ -0,0 +1,30 @@
+package awsclient
+
+import (
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+var Session *session.Session
+
+func InitSession() (_ *session.Session, err error) {
+	Session, err = session.NewSession(
+		&aws.Config{
+			Region: aws.String(os.Getenv("AWS_REGION")),
+			Credentials: credentials.NewStaticCredentials(
+				os.Getenv("AWS_ACCESS_KEY_ID"),
+				os.Getenv("AWS_SECRET_ACCESS_KEY"),
+				"",
+			),
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return Session, nil
+}
