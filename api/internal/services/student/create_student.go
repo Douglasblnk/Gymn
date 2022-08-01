56068dafa8c6e79dcf78b9df6e7a05c6290846f2
Douglas Blank <douglas.blank@multiplier.com.br>
2022-08-01 14:29:04 -0300

    feat: implement code gen to student struct

--- api/internal/services/student/create_student.go
+++ api/internal/services/student/create_student.go
@@ -1,6 +1,7 @@
-package userService
+package studentService
 
 import (
+	"gymn/internal/dto"
 	"gymn/internal/models"
 	studentRepository "gymn/internal/repository/student"
 	"gymn/internal/utils"
@@ -8,20 +9,35 @@ import (
 	"time"
 )
 
-func CreateStudent(data *schemas.RegisterStudent) (*models.Student, *utils.Error) {
+func CreateStudent(data *schemas.RegisterStudent) (*dto.StudentDTO, *utils.Error) {
 	student := &models.Student{
 		FirstName: data.FirstName,
 		LastName:  data.LastName,
 		Birth:     data.Birth,
 		Color:     data.Color,
-		StartDate: time.Now().Format("01-02-2006"),
-		Weight:    data.Weight,
-		Height:    data.Height,
+		Code:      utils.GenerateRandomText(24),
+		StartDate: (func() string {
+			if data.StartDate != nil {
+				return *data.StartDate
+			}
+
+			return time.Now().Format("01-02-2006")
+		})(),
+		Weight: data.Weight,
+		Height: data.Height,
 	}
 
 	if err := studentRepository.CreateStudent(student); err != nil {
 		return nil, err
 	}
 
-	return student, nil
+	return &dto.StudentDTO{
+		FirstName: student.FirstName,
+		LastName:  student.LastName,
+		Birth:     student.Birth,
+		Color:     student.Color,
+		StartDate: student.StartDate,
+		Weight:    student.Weight,
+		Height:    student.Height,
+	}, nil
 }
