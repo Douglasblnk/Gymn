dad64ab50c5df41fffc79a7f46689e44a92d7380
Douglas Blank <douglasblank1@gmail.com>
2022-08-07 18:57:30 -0300

    feat: implement visual login page

--- app/src/components.d.ts
+++ app/src/components.d.ts
@@ -7,9 +7,13 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    GButton: typeof import('./components/atoms/GButton.vue')['default']
+    GFadeTransition: typeof import('./components/atoms/GFadeTransition.vue')['default']
+    GIcon: typeof import('./components/atoms/GIcon.vue')['default']
+    GInputText: typeof import('./components/atoms/GInputText.vue')['default']
     GTitle: typeof import('./components/atoms/GTitle.vue')['default']
+    Icon: typeof import('./components/atoms/Icon.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    Undefined: typeof import('./pages/index.vue')['default']
   }
 }
