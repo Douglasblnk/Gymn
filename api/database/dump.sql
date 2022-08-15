afc4a4d0fd64253499ac7b7c6c931fc83f5552fd
Douglas Blank <douglas.blank@multiplier.com.br>
2022-08-15 10:39:31 -0300

    feat: improved relations between tables

--- api/database/dump.sql
+++ api/database/dump.sql
@@ -18,18 +18,14 @@ CREATE TABLE "sessions" (
 	"refresh_token" CHARACTER varying NOT NULL UNIQUE,
 	"created_at" TIMESTAMP NOT NULL DEFAULT now(),
 	"updated_at" TIMESTAMP NOT NULL DEFAULT now(),
-	"deleted_at" TIMESTAMP
-);
-
-CREATE TABLE "user_student" (
-  "user_id" INT,
-  "student_id" INT,
-  PRIMARY KEY ("user_id", "student_id")
+	"deleted_at" TIMESTAMP,
+  FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE
 );
 
 CREATE TABLE "students" (
   "id" SERIAL PRIMARY KEY,
   "uid" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "user_id" INT NOT NULL,
   "first_name" VARCHAR NOT NULL,
   "last_name" VARCHAR NOT NULL,
   "birth" DATE,
@@ -40,13 +36,8 @@ CREATE TABLE "students" (
   "height" VARCHAR NOT NULL,
   "created_at" TIMESTAMP NOT NULL DEFAULT now(),
   "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
-  "deleted_at" TIMESTAMP
-);
-
-CREATE TABLE "student_training_sheets" (
-  "student_id" INT,
-  "training_sheet_id" INT,
-  PRIMARY KEY ("student_id", "training_sheet_id")
+  "deleted_at" TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE "training_sheets" (
@@ -59,26 +50,12 @@ CREATE TABLE "training_sheets" (
   "deleted_at" TIMESTAMP
 );
 
-CREATE TABLE "training_sheets_workouts" (
+CREATE TABLE "student_training_sheets" (
+  "student_id" INT,
   "training_sheet_id" INT,
-  "workout_id" INT,
-  PRIMARY KEY ("training_sheet_id", "workout_id")
-);
-
-CREATE TABLE "workouts" (
-  "id" SERIAL PRIMARY KEY,
-  "uid" uuid NOT NULL DEFAULT gen_random_uuid(),
-  "exercise" VARCHAR NOT NULL,
-  "equipment" INT,
-  "description" VARCHAR,
-  "repetitions" VARCHAR,
-  "series" INT,
-  "rest_time" INT,
-  "weight" float,
-  "cadence" VARCHAR,
-  "created_at" TIMESTAMP NOT NULL DEFAULT now(),
-  "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
-  "deleted_at" TIMESTAMP
+  PRIMARY KEY ("student_id", "training_sheet_id"),
+  FOREIGN KEY ("student_id") REFERENCES "students" ("id") ON UPDATE CASCADE,
+  FOREIGN KEY ("training_sheet_id") REFERENCES "training_sheets" ("id") ON UPDATE CASCADE
 );
 
 CREATE TABLE "equipment" (
@@ -87,16 +64,27 @@ CREATE TABLE "equipment" (
   "name" VARCHAR
 );
 
-ALTER TABLE "user_student" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+CREATE TABLE "workouts" (
+  "id" SERIAL PRIMARY KEY,
+  "uid" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "exercise" VARCHAR NOT NULL,
+  "equipment_id" INT,
+  "description" VARCHAR,
+  "repetitions" VARCHAR,
+  "series" INT,
+  "rest_time" INT,
+  "weight" float,
+  "cadence" VARCHAR,
+  "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+  "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
+  "deleted_at" TIMESTAMP,
+  FOREIGN KEY ("equipment_id") REFERENCES "equipment" ("id") ON UPDATE CASCADE
+);
 
-ALTER TABLE "user_student" ADD FOREIGN KEY ("student_id") REFERENCES "students" ("id");
-
-ALTER TABLE "student_training_sheets" ADD FOREIGN KEY ("student_id") REFERENCES "students" ("id");
-
-ALTER TABLE "student_training_sheets" ADD FOREIGN KEY ("training_sheet_id") REFERENCES "training_sheets" ("id");
-
-ALTER TABLE "training_sheets_workouts" ADD FOREIGN KEY ("training_sheet_id") REFERENCES "training_sheets" ("id");
-
-ALTER TABLE "training_sheets_workouts" ADD FOREIGN KEY ("workout_id") REFERENCES "workouts" ("id");
-
-ALTER TABLE "sessions" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+CREATE TABLE "training_sheets_workouts" (
+  "training_sheet_id" INT,
+  "workout_id" INT,
+  PRIMARY KEY ("training_sheet_id", "workout_id"),
+  FOREIGN KEY ("training_sheet_id") REFERENCES "training_sheets" ("id") ON UPDATE CASCADE,
+  FOREIGN KEY ("workout_id") REFERENCES "workouts" ("id") ON UPDATE CASCADE
+);
